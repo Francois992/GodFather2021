86960372a76d1d232d27c09f3ee1811d3cf4e466
Maxence-Dupin <maxencedupin001@gmail.com>
2021-09-10 09:39:16 +0200

    Update UI

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,7 +10,7 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-      SceneManager.LoadScene("Game");
+      SceneManager.LoadScene("MainScene");
     }
 
     public void OpenControls()
